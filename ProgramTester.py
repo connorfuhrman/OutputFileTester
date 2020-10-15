fffa4d9491b26bc7018a7377bc1b07a7697d8d49
connorfuhrman <connorfuhrman@outlook.com>
2020-10-15 09:57:09 -0700

    Updated to compare between output files [again]

--- ProgramTester.py
+++ ProgramTester.py
@@ -111,12 +111,30 @@ class ProgramTester:
         out = self.execute_program(self.get_command_arguments(solution_filename))
         return self.compare_output(out, self.read_output_file("solution/{}".format(solution_filename)))
         
+    def remove_text_inside_brackets(self, text, brackets="()[]"):
+        count = [0] * (len(brackets) // 2) # count open/close brackets
+        saved_chars = []
+        for character in text:
+            for i, b in enumerate(brackets):
+                if character == b: # found bracket
+                    kind, is_close = divmod(i, 2)
+                    count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                    if count[kind] < 0: # unbalanced bracket
+                        count[kind] = 0  # keep it
+                    else:  # found bracket to remove
+                        break
+            else: # character is not a [balanced] bracket
+                if not any(count): # outside brackets
+                    saved_chars.append(character)
+        return ''.join(saved_chars)
+        
     def test_program_outputFiles(self, i):
         # Get the command line arguments from ./solution/command_arguments.txt
         with open("solution/command_arguments.txt") as f:
             # Get the ith line of the file
             lines = f.readlines()
-            l = re.sub("[\(\[].*?[\)\]]", "", lines[i]).strip()
+            l = self.remove_text_inside_brackets(lines[i]).strip()
+            print(l)
         # Execute the student's program with the given command line arguments
         out = self.execute_program(l.format("studentOutput/", i))
         # Compare between the two files
